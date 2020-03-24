d7b1cf59b020151c7c0d124a3c18836b0959ac85
Randeep Singh <ransingh@ciena.com>
2020-03-24 19:50:20 -0400

    get distinct countries collection

--- src/apis/bikeNetworksApis.js
+++ src/apis/bikeNetworksApis.js
@@ -6,3 +6,58 @@ async function getBikeNetworksAsync() {
     // handle and log error
   }
 }
+
+export async function getDistinctCountries() {
+  let bikeNetworks = await getBikeNetworksAsync();
+  let countries = [];
+
+  bikeNetworks.networks.forEach(network => {
+    let countryIndex = countries.filter(
+      c => c.code === network.location.country
+    );
+    if (countryIndex.length === 0) {
+      let country = {};
+      country.code = network.location.country;
+
+      let city = {};
+      city.name = network.location.city;
+      city.networks = [];
+      city.networks.push(network);
+
+      if (country.cities === undefined) {
+        country.cities = [];
+      }
+
+      country.cities.push(city);
+      countries.push(country);
+    } else {
+      if (countryIndex[0].cities === undefined) {
+        let city = {};
+        city.name = network.location.city;
+        city.networks = [];
+        city.networks.push(network);
+
+        countryIndex[0].cities.push(city);
+      } else {
+        let filteredCity = countryIndex[0].cities.filter(
+          city => city.name === network.location.city
+        );
+        if (filteredCity.length === 0) {
+          let city = {};
+          city.name = network.location.city;
+          city.networks = [];
+          city.networks.push(network);
+
+          countryIndex[0].cities.push(city);
+        } else {
+          if (filteredCity[0].networks === undefined) {
+            filteredCity[0].networks = [];
+          }
+          filteredCity[0].networks.push(network);
+        }
+      }
+    }
+  });
+
+  return countries;
+}
